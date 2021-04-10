3afd7cda4b547549b6c46119efb5985960ca24c2
Om Nom <omnom62@outlook.com>
2021-04-10 22:12:52 +1000

    synth

--- app.py
+++ app.py
@@ -10,9 +10,10 @@ from sg_ec2_rds_alb.sg_rds_stack import SG_RDS
 
 app = core.App()
 
-# env = core.Environment(account="107161814468", region="ap-southeast-2")
+#env = core.Environment(account="107161814468", region="ap-southeast-2")
 
 vpc_stack = SG_VPC(app, "cdk-vpc", env=env)
 ec2_stack = SG_EC2(app, "cdk-ec2", env=env, vpc=vpc_stack.vpc)
 rds_stack = SG_RDS(app, "cdk-rds", env=env, vpc=vpc_stack.vpc)
+
 app.synth()
